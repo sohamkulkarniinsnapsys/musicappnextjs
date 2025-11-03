1f7d64a03e9395607f74f23ce65fa5e1137c8893
Soham Kulkarni <sohamkulkarni.insnapsys@gmail.com>
2025-11-03 18:37:45 +0530

    footer changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -74,13 +74,13 @@ function Footer() {
         </div>
         <div>
           <h2 className="text-white text-lg font-semibold mb-4">Contact Us</h2>
-          <p>New Delhi, India</p>
+          <p>India</p>
           <p>Delhi 10001</p>
-          <p>Email: info@musicschool.com</p>
+          <p>Email: info@musicappnextjs.com</p>
           <p>Phone: (123) 456-7890</p>
         </div>
         </div>
-        <p className="text-center text-xs pt-8">© 2024 Music School. All rights reserved.</p>
+        <p className="text-center text-xs pt-8">© 2025 Music. All rights reserved.</p>
     </footer>
   )
 }
